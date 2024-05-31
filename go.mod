f3f347b9cb8575ca06be74fb365bec6abca42719
the0ne <sosiso4ka@kolbasa.ru>
2024-05-31 18:40:08 +0300

    asda

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.22.3
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/The0ne10/protos v0.0.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/grpc v1.64.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,8 +2,14 @@ package auth
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
+	"sso/internal/lib/jwt"
+	"sso/internal/lib/sl"
+	"sso/internal/storage"
 	"time"
 )
 
@@ -32,6 +38,10 @@ type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
 }
 
+var (
+	ErrInvalidCredentials = errors.New(`invalid credential`)
+)
+
 // New returns a new instance of the Auth service
 func New(
 	log *slog.Logger,
@@ -49,14 +59,97 @@ func New(
 	}
 }
 
-func (a *Auth) Login(ctx context.Context, email string, password string) (string, error) {
-	panic("implement me")
+func (a *Auth) Login(ctx context.Context, email string, password string, appID int) (string, error) {
+	const op = "auth.login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("attempting to login user")
+
+	user, err := a.usrProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+		a.log.Error("failed to get user", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Info("invalid password", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to create token", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
 }
 
-func (a *Auth) Register(ctx context.Context, email string, password string) (string, error) {
-	panic("implement me")
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (int64, error) {
+	const op = "auth.registerNewUser"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("registering new user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to hash password", sl.Err(err))
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user", sl.Err(err))
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user created")
+
+	return id, nil
+
 }
 
 func (a *Auth) IsAdmin(ctx context.Context, userId int64) (bool, error) {
-	panic("implement me")
+	const op = "auth.isAdmin"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int64("user_id", userId),
+	)
+
+	log.Info("checking if user is admin")
+
+	isAdmin, err := a.usrProvider.IsAdmin(ctx, userId)
+	if err != nil {
+		return false, fmt.Errorf("%s, %w", op, err)
+	}
+
+	log.Info("checked if user is admin", slog.Bool("is_admin", isAdmin))
+
+	return isAdmin, nil
 }
